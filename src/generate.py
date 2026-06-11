9e70a9f29903c76358b0618cbdf7716c58f9322d
Sang Yeop <piegeek@Sangs-MBP.com>
2026-06-11 13:32:03 +0900

    Updated generate.py to start generating using BPE tokenizer

--- src/generate.py
+++ src/generate.py
@@ -3,21 +3,31 @@ import torch
 
 from src.dataset import CharDataset
 from src.model import TinyLM, AttentionLM
+from src.tokenizer import BPETokenizer
 
-with open('data/data.txt', 'r') as f:
-	text = f.read()
+tokenizer = BPETokenizer()
 
-dataset = CharDataset(text)
-vocab_size = dataset.vocab_size
+vocab_size = tokenizer.vocab_size
+
+# Hyperparameters
+context_length = 64
+batch_size = 32
+d_model = 64
+learning_rate = 3e-4
+n_heads = 1
+n_layers = 1
+epochs = 10
 
 model = AttentionLM(
 	vocab_size=vocab_size,
-	d_model=64,
-	n_layers=4
+	d_model=d_model,
+	context_length=context_length,
+	num_heads=n_heads,
+	n_layers=n_layers
 )
 
 model.load_state_dict(
-	torch.load('model.pt')
+	torch.load('checkpoint.pt')
 )
 
 model.eval()
@@ -49,20 +59,23 @@ def generate(model, idx, max_new_tokens, temperature, context_length):
 
 	return idx
 
-start = torch.tensor([
-	[dataset.stoi['h']]
-])
+prompt = 'hello'
+
+start_tokens = tokenizer.encode(prompt)
+
+start = torch.tensor(
+	[start_tokens],
+	dtype=torch.long
+)
 
 tokens = generate(
 	model,
 	start,
-	max_new_tokens=1,
+	max_new_tokens=50,
 	temperature=0.8,
-	context_length=8
+	context_length=context_length
 )
 
-decoded = ''.join(
-	dataset.itos[i.item()] for i in tokens[0]
-)
+decoded = tokenizer.decode(tokens[0].tolist())
 
 print(decoded)
